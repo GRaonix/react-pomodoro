9a1920ee5252430458d6cea35cfa78d1832ac425
raoni.gillet@gmail.com <raoni.gillet@gmail.com>
2021-11-16 10:55:25 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+export function App() {
+    const name = "Pomodoro"
+    return {
+        <>
+        <h1>Hello {name}!</h1>
+        <div className={"timer"}>
+
+
+        </div></>
+
+
+
+    };
+}
